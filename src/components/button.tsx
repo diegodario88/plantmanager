390cee9fa8bee501adda8e8a036e9342d34a60bf
Diego Dario <diegodario88@gmail.com>
2021-04-25 12:59:37 -0300

    feat: Handler on button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import { TouchableOpacity, Text, TextStyle } from 'react-native';
 import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 
 interface ButtonProps {
   title?: string;
   titleFontSize?: number;
   titleMarginTop?: number;
-  height: number;
-  width: number;
-  icon: JSX.Element;
+  titleColor?: string;
+  height: number | string;
+  width: number | string;
+  icon?: JSX.Element;
+  onPress?: () => void;
 }
 
 export default function Button(props: ButtonProps): JSX.Element {
-  const { height, width, title, titleFontSize, icon, titleMarginTop } = props;
+  const {
+    height,
+    width,
+    title,
+    titleFontSize,
+    icon,
+    titleMarginTop,
+    titleColor,
+    onPress,
+  } = props;
 
   return (
-    <TouchableOpacity style={styles.button(height, width)}>
-      <Text style={styles.title(titleFontSize ?? 0, titleMarginTop ?? 0)}>
+    <TouchableOpacity style={styles.button(height, width)} onPress={onPress}>
+      <Text
+        style={styles.title(
+          titleFontSize ?? 0,
+          titleMarginTop ?? 0,
+          titleColor ?? colors.heading
+        )}
+      >
         {title}
       </Text>
       {icon}
@@ -25,7 +43,7 @@ export default function Button(props: ButtonProps): JSX.Element {
 }
 
 const styles = {
-  button: (height: number, width: number): TextStyle => ({
+  button: (height: number | string, width: number | string): TextStyle => ({
     backgroundColor: colors.green,
     justifyContent: 'center',
     alignItems: 'center',
@@ -34,9 +52,14 @@ const styles = {
     height: height,
     width: width,
   }),
-  title: (fontSize: number, marginTop: number): TextStyle => ({
+  title: (
+    fontSize: number,
+    marginTop: number,
+    titleColor: string
+  ): TextStyle => ({
     fontSize: fontSize,
     marginTop: marginTop,
-    color: colors.heading,
+    color: titleColor,
+    fontFamily: fonts.heading,
   }),
 };
